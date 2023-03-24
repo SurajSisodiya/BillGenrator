02327b93c1f39d2074665c37c8fe8facceb01405
Suraj Mal <surajsisodiya5889@gmail.com>
2023-03-24 17:36:13 +0530

    First Commit

---
+++ EBill/Repository/IData.cs
@@ -0,0 +1,16 @@
+﻿using EBill.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+namespace EBill.Repository
+{
+    interface IData
+    {
+        void SaveBillDetail(BillDetail details);
+        void SaveBIllItems(List<Items> items, SqlConnection con, int id);
+        List<BillDetail> GetAllDetail();
+        BillDetail GetDetail(int Id);
+    }
+}
